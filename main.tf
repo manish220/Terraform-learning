edc4fd28dd40af9f4953b104bee41308005ab4da
Manish Mistry <67853128+manish220@users.noreply.github.com>
2025-11-06 18:15:40 +0000

    implemented modules

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_value
-  instance_type = var.instance_type_value
+module "ec2_instance" {
+    source = "./module/ec2_instance"
+    ami_value = "ami-0157af9aea2eef346"
+    instance_type_value = "t2.micro"
+  
 }
