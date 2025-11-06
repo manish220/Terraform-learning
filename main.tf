04e1fe0213e747271a73e42eafbd59c5bd879c38
Manish Mistry <67853128+manish220@users.noreply.github.com>
2025-11-06 18:00:43 +0000

    addind var and output

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0157af9aea2eef346"
-  instance_type = "t2.micro"
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
 }
